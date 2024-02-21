113a2f9fc13c7c24eb981bb9591cedda4101c949
Divvi221 <divya.kranthi@ucsf.edu>
2024-02-21 08:30:54 -0800

    added forward function in the hmm class

--- hmm/hmm.py
+++ hmm/hmm.py
@@ -42,12 +42,36 @@ class HiddenMarkovModel:
         """        
         
         # Step 1. Initialize variables
+        T = len(input_observation_states) #["sunny","rainy","rainy","sunny","rainy"]
+        N = len(self.hidden_states) #hot and cold are hidden states
+        #print(input_observation_states) 
+        forward = np.zeros((N,T)) #initial forward prob matrix 
         
-       
+        #initialize forward probabilities:
+        for s in range(N):
+            obs_ind = np.where(self.observation_states == input_observation_states[0])[0][0] #find index of the first input obs states in the list of obs states 
+            #print(obs_ind)
+            forward[s,0] = self.prior_p[s] * self.emission_p[s,obs_ind]
+        #print(forward)
         # Step 2. Calculate probabilities
-
-
+        for t in range(1,T):
+            obs_ind = np.where(self.observation_states == input_observation_states[t])[0][0] #index of the observed output in the hidden state list
+            #print(obs_ind)
+            for s in range(N):
+                #forward[s,t] = 
+                sum = 0
+                for s_p in range(N):
+                    elem = forward[s_p,t-1] * self.transition_p[s_p,s] * self.emission_p[s_p,obs_ind]
+                    #if s_p==0:
+                        #print(self.emission_p[s_p,obs_ind])
+                    sum += elem
+                forward[s,t] = sum
+        #print(forward)
         # Step 3. Return final probability 
+        forward_prob = 0
+        for s in range(N):
+            forward_prob += forward[s,T-1]
+        return forward_prob
         
 
 
@@ -80,3 +104,9 @@ class HiddenMarkovModel:
 
         # Step 4. Return best hidden state sequence 
         
+#testing and debugging
+mini_hmm=np.load('./data/mini_weather_hmm.npz')
+mini_hmm_seq=np.load('./data/mini_weather_sequences.npz')
+#print(mini_hmm.keys())
+hmm = HiddenMarkovModel(mini_hmm["observation_states"],mini_hmm["hidden_states"],mini_hmm["prior_p"],mini_hmm["transition_p"],mini_hmm["emission_p"])
+hmm_forward = hmm.forward(mini_hmm_seq["observation_state_sequence"])
